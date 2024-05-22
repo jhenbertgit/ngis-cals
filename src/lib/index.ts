df382ffaf5d9756c9f06009bb597ada13b6c74d1
Jhenbert Villamucho <jvv205756@gmail.com>
2024-05-22 12:58:03 +0800

    first commit

---
+++ src/lib/index.ts
@@ -0,0 +1,16 @@
+// place files you want to import through the `$lib` alias in this folder.
+export const calcPOCCE = () => {};
+
+export const calcPSCCE = () => {};
+
+export const calcPOCR = () => {};
+
+export const calcPSCR = () => {};
+
+export const calcAMPOCR = () => {};
+
+export const calcAMPSCR = () => {};
+
+export const calcPOCSE = () => {};
+
+export const calcPSCSE = () => {};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<div class="container h-full mx-auto flex justify-center items-start gap-7 mt-5">
+	<div class="w-1/2 space-y-5">
+		<h1 class="h1">Calculator</h1>
+		<div class="grid grid-cols-2 gap-4">
+			<button type="button" class="btn btn-xl variant-filled">Calculate POCCE</button>
+			<button type="button" class="btn btn-xl variant-filled">Calculate PSCCE</button>
+			<button type="button" class="btn btn-xl variant-filled">Calculate POCR</button>
+			<button type="button" class="btn btn-xl variant-filled">Calculate PSCR</button>
+			<button type="button" class="btn btn-xl variant-filled">Calculate AMPOCR</button>
+			<button type="button" class="btn btn-xl variant-filled">Calculate AMPSCR</button>
+			<button type="button" class="btn btn-xl variant-filled">Calculate POCSE</button>
+			<button type="button" class="btn btn-xl variant-filled">Calculate PSCSE</button>
+		</div>
+	</div>
+	<div class="flex flex-col items-center space-y-5">
+		<h3 class="h3">Input data</h3>
+		<div class="card p-5">
+			<label class="label"><span>POI</span><input class="input" type="text" name="poi" /> </label>
+			<label class="label"><span>PSI</span><input class="input" type="text" name="psi" /> </label>
+			<label class="label"
+				><span>Population</span><input class="input" type="text" name="population" />
+			</label>
+			<label class="label"
+				><span>Crime Solved</span><input class="input" type="text" name="solved" />
+			</label>
+			<label class="label"
+				><span>Crime Cleared</span><input class="input" type="text" name="cleared" />
+			</label>
+			<label class="label"
+				><span>No. of Months</span><input class="input" type="text" name="months" />
+			</label>
+		</div>
+	</div>
+</div>
